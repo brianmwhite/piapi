1be6ed446ccb11876848d0ebcfe1aaf5377f69dc
Brian White <brian@thirdember.com>
2020-10-03 14:41:19 -0400

    add sleep services for just bedroom, add unmute office

--- app.py
+++ app.py
@@ -281,8 +281,44 @@ def sonos_wake_owen():
     
     return '{"status":"success"}'
 
+@app.route("/sonos/sleep/bedroom")
+def sonos_sleep_bedroom():
+
+    if sonos_api_check_if_beachisplaying(SONOS_OWENS_ROOM):
+        sonos_api_call("[bedroom] pause", f"{SONOS_API_URL}/{SONOS_BEDROOM}/pause")
+        sonos_api_call("[bedroom] ungroup", f"{SONOS_API_URL}/{SONOS_BEDROOM}/leave")
+        sonos_api_call("[bedroom] set volume", f"{SONOS_API_URL}/{SONOS_BEDROOM}/volume/40")       
+        sonos_api_call("[bedroom] join owen's room",f"{SONOS_API_URL}/{SONOS_BEDROOM}/join/{SONOS_OWENS_ROOM}")
+    else:
+        sonos_api_call("[bedroom] pause", f"{SONOS_API_URL}/{SONOS_BEDROOM}/pause")       
+        sonos_api_call("[bedroom] ungroup", f"{SONOS_API_URL}/{SONOS_BEDROOM}/leave")
+        sonos_api_call("[bedroom] set volume", f"{SONOS_API_URL}/{SONOS_BEDROOM}/volume/40")
+        sonos_api_call("[bedroom/group] start Sleep playlist", f"{SONOS_API_URL}/{SONOS_BEDROOM}/playlist/Sleep")
+
+        sonos_api_repeat(SONOS_BEDROOM, STATE_ON)
+        sonos_api_crossfade(SONOS_BEDROOM, STATE_ON)
+    
+    return '{"status":"success"}'
+
+@app.route("/sonos/wake/bedroom")
+def sonos_wake_bedroom():
+    sonos_api_call("[bedroom] pause", f"{SONOS_API_URL}/{SONOS_BEDROOM}/pause")
+    sonos_api_call("[bedroom] ungroup", f"{SONOS_API_URL}/{SONOS_BEDROOM}/leave")
+    sonos_api_call("[bedroom] set volume", f"{SONOS_API_URL}/{SONOS_BEDROOM}/volume/20")
+    
+    sonos_api_repeat(SONOS_BEDROOM, STATE_OFF)
+    sonos_api_crossfade(SONOS_BEDROOM, STATE_OFF)
+    
+    return '{"status":"success"}'
+
+
 @app.route("/sonos/officestop")
 def sonos_office_stop():
     sonos_api_call("[office] mute", f"{SONOS_API_URL}/{SONOS_OFFICE}/mute")
     sonos_api_call("[office] ungroup", f"{SONOS_API_URL}/{SONOS_OFFICE}/leave")
+    return '{"status":"success"}'
+
+@app.route("/sonos/officeunmute")
+def sonos_office_unmute():
+    sonos_api_call("[office] unmute", f"{SONOS_API_URL}/{SONOS_OFFICE}/unmute")
     return '{"status":"success"}'
